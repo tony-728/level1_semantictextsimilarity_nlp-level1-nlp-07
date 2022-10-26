630993194a7c61640ee9c18c15ea12ac3517cc17
tony <59431433+tony-728@users.noreply.github.com>
2022-10-26 15:59:45 +0900

    model checkpoint file name 수정

--- train.py
+++ train.py
@@ -252,7 +252,7 @@ if __name__ == "__main__":
     checkpoint_callback = ModelCheckpoint(
         monitor="val_pearson",
         dirpath=project_name,
-        filename="{epoch:02d}-{val_loss:.2f}",
+        filename="{epoch:02d}-{val_pearson:.2f}",
         save_top_k=3,
         mode="min",
     )
